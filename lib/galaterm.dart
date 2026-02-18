f1c3c3462891924f4b41a745348c976fad9fb8dd
Jake MacDonald <jakemac53@gmail.com>
2026-02-18 13:41:21 -0800

    Implement mouse-based player movement and increase grid size

--- lib/galaterm.dart
+++ lib/galaterm.dart
@@ -8,6 +8,12 @@ class GalatermApp extends StatefulComponent {
 }
 
 class _GalatermAppState extends State<GalatermApp> {
+  int _playerX = 40;
+  int _playerY = 20;
+
+  final int _width = 80;
+  final int _height = 40;
+
   @override
   Component build(BuildContext context) {
     return NoctermApp(
@@ -26,15 +32,42 @@ class _GalatermAppState extends State<GalatermApp> {
             decoration: BoxDecoration(
               border: BoxBorder.all(style: BoxBorderStyle.rounded),
             ),
-            child: const Padding(
-              padding: EdgeInsets.all(2),
+            child: Padding(
+              padding: const EdgeInsets.all(1),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Text('Welcome to Galaterm!'),
-                  SizedBox(height: 1),
-                  Text(
-                    'Press "q" to quit.',
+                  ...List.generate(_height, (y) {
+                    return Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: List.generate(_width, (x) {
+                        final isPlayer = (x == _playerX && y == _playerY);
+                        return MouseRegion(
+                          onHover: (event) {
+                            if (_playerX != x || _playerY != y) {
+                              setState(() {
+                                _playerX = x;
+                                _playerY = y;
+                              });
+                            }
+                          },
+                          // We also use onEnter just to be sure we catch the initial grid entry
+                          onEnter: (event) {
+                            if (_playerX != x || _playerY != y) {
+                              setState(() {
+                                _playerX = x;
+                                _playerY = y;
+                              });
+                            }
+                          },
+                          child: Text(isPlayer ? '▲' : ' '),
+                        );
+                      }),
+                    );
+                  }),
+                  const SizedBox(height: 1),
+                  const Text(
+                    'Use mouse to move. Press "q" to quit.',
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ],
