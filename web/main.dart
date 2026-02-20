d6e09454ff5bc20eb51128cf87dcf398137934c6
Jake MacDonald <jakemac53@gmail.com>
2026-02-20 14:03:53 -0800

    remove accidental boilerplate in web/main.dart

--- web/main.dart
+++ web/main.dart
@@ -1,21 +1,6 @@
-import 'dart:io';
 import 'package:galaterm/galaterm.dart';
 import 'package:nocterm/nocterm.dart';
 
 void main() async {
   await runApp(const GalatermApp());
-  await _flushThenExit(0);
-}
-
-/// Flushes the stdout and stderr streams, then exits the program with the given
-/// status code.
-///
-/// This returns a Future that will never complete, since the program will have
-/// exited already. This is useful to prevent Future chains from proceeding
-/// after you've decided to exit.
-Future<void> _flushThenExit(int status) {
-  return Future.wait<void>([
-    stdout.close(),
-    stderr.close(),
-  ]).then<void>((_) => exit(status));
 }
